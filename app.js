97e57e1ecbdd88d31b9860bca7d0a71a40cf0d86
LorenzoScalvini <lorenzo.scalvini1704@gmail.com>
2024-11-20 14:08:41 +0100

    ADD: app.js main file

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('ciaoooo')
+console.log('Hello World :D')
+const express = require('express')
+const postsRouter = require('./routers/posts.js')
+const app = express()
+const port = 3017
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    console.log('Questa e\' la root!')
+    res.send('Hello World :D')
+})
+
+app.use('/posts', postsRouter)
+
+app.listen(port, () => {
+    console.log(`il server sta ascoltando sulla porta: ${port}`)
+})

ea4421f4723fc1e074933c7f577da1f0b621070f
LorenzoScalvini <lorenzo.scalvini1704@gmail.com>
2024-11-20 14:26:52 +0100

    ADD: fix id error

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -7,6 +7,7 @@ function index (req, res) {
 
 //show function
 function show (req, res) {
+    const id = req.params.id
     console.log(`Ecco il post con id: ${id}`) 
 }
 
@@ -27,6 +28,7 @@ function modify (req, res) {
 
 //destroy function
 function destroy (req, res) {
+    const id = req.params.id
     console.log(`Elimino la pizza con id: ${id}`)
 }
 

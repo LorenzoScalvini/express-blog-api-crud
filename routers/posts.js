835631fa3f7ed24d0593b93ec93f457d788b3ec1
LorenzoScalvini <lorenzo.scalvini1704@gmail.com>
2024-11-20 14:13:26 +0100

    ADD: routers main file

---
+++ routers/posts.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const router = express.router()
+const postsController = require('../controllers/postsController.js')
+
+//index router function
+router.get('/', postsController.index) 
+
+//show router function
+router.get('/:id', postsController.show)
+
+//store router function
+
+//update router function
+
+//modify router function
+
+//destroy router function
+router.delete('/:id', postsController.destroy)
+
+module.exports = router

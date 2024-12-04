e08695d9e8e7c8cc15bad8c166a3d3ff32533b23
LorenzoScalvini <lorenzo.scalvini1704@gmail.com>
2024-12-04 15:43:23 +0100

    ADD: implementazione cors

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
-console.log('Hello World :D');
-const express = require('express');
-const postsRouter = require('./routers/posts.js');
-const { notFoundHandler, errorHandler } = require('./middlewares/errorHandlers');
+console.log("Hello World :D");
+const express = require("express");
+const cors = require("cors");
+const postsRouter = require("./routers/posts.js");
+const {
+  notFoundHandler,
+  errorHandler,
+} = require("./middlewares/errorHandlers");
 const app = express();
 const port = 3017;
 
+app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-    console.log('Questa e\' la root!');
-    res.send('Hello World :D');
+app.get("/", (req, res) => {
+  console.log("Questa e' la root!");
+  res.send("Hello World :D");
 });
 
-app.use('/posts', postsRouter);
+app.use("/posts", postsRouter);
 
 app.use(notFoundHandler);
 
 app.use(errorHandler);
 
 app.listen(port, () => {
-    console.log(`Il server sta ascoltando sulla porta: ${port}`);
+  console.log(`Il server sta ascoltando sulla porta: ${port}`);
 });
